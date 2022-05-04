2ad2e907867d9a6c54b7bf00f20c7e86bc2d8849
Felipe Gené <felipegenef@gmail.com>
2022-05-04 15:32:03 -0300

    hit target example

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let nextLevel = "index.html";
 const shoot = () => {
   const bullet = document.createElement("a-sphere");
   let pos = myCamera.getAttribute("position");
-  console.log(getDirection.toString());
   bullet.setAttribute("position", pos);
   bullet.setAttribute("velocity", getDirection(myCamera, 50));
   bullet.setAttribute("dynamic-body", true);
-  bullet.setAttribute("radius", 0.001);
+  bullet.setAttribute("radius", 0.1);
   bullet.setAttribute("src", "");
   bullet.setAttribute("transparent", true);
   myScene.appendChild(bullet);
@@ -27,16 +26,6 @@ const shootCollided = (event) => {
   }
   if (document.querySelectorAll(".target").length === 0) {
     console.log("You win!");
-  }
-};
-
-const teleportCollided = (event) => {
-  if (event.detail.body.el.id === "floor") {
-    console.log("Hit the floor");
-    event.detail.target.el.removeEventListener("collide", teleportCollided);
-    let pos = event.detail.target.el.getAttribute("position");
-    myScene.removeChild(event.detail.target.el);
-    myCamera.setAttribute("position", { x: pos.x, y: 2, z: pos.z });
   }
 };
 
