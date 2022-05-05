acf023c4a2d083144abef7a43c476fb9396ff063
Felipe Gené <felipegenef@gmail.com>
2022-05-05 17:04:38 -0300

    add new

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const wonAudio = new Audio("assets/won.mp3");
 //   }
 // }, 1000);
 let currentPosition = "0 1 0";
+myCamera.setAttribute("static-body", true);
 myCamera.addEventListener("collide", (event) => {
   console.log(event);
 });
@@ -153,7 +154,6 @@ const won = () => {
 };
 let isWalkingAudioOn = false;
 document.onkeydown = (event) => {
-  console.log(event);
   if (first) {
     first = false;
 
